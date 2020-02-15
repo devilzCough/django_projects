a763be999dadc7b278ab63b2debf6325db884080
DevilzCough <devilzcough@gmail.com>
2020-02-15 20:39:14 +0900

    --

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,7 @@
 # app_name/urls.py
-    from django.urls import path
-    from . import views
+from django.urls import path
+from . import views
 
-    urlpatterns = [
-        path('', views.post_list, name='post_list'),
-    ]
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
